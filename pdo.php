ec46e0ce5745420fbc9581961b7284f9c3f92286
R-anema <48018519+R-anema@users.noreply.github.com>
2022-03-07 23:48:57 +0100

    Add files via upload

--- pdo.php
+++ pdo.php
@@ -1,23 +1,370 @@
 <?php
+//de database en functies voor create en delete zitten in de DB class.
+class Database {
 
-$host = 'localhost';
-$db   = 'rentacar';
-$user = 'root';
-$pass = '';
-$charset = 'utf8mb4';
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
+private $host = '127.0.0.1';
+private $user = 'root';
+private $pass = '';
+private $db = 'tennisbond';
+private $charset = 'utf8mb4';
 
-try {
-    $dbh = new PDO("mysql:host=$host;dbname=media", $user, $pass);
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e) {
-    throw new PDOException($e->getMessage(), (int)$e->getCode());
+private $pdo;
+
+/* private $options = [
+    PDO::ATTR_ERRMODE               => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE    => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES      => FALSE,
+]; */
+
+//construct verbinden met de DB
+    public function __construct(string $host = 'localhost', string $user = 'root', string $pass = '', string $db = 'tennisbond', string $charset = 'utf8'){
+        $this->host = $host;
+        $this->user = $user;
+        $this->pass = $pass;
+        $this->db = $db;
+        $this->charset = $charset;
+
+        try {
+            $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+            $conn = new PDO("mysql:host=$host;dbname=tennisbond;charset=$charset", $user,$pass);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->pdo = new PDO($dsn, $user, $pass, /*$options*/);
+        } catch (\PDOException $e) {
+            throw new PDOException($e-getMessage(), (int)$e->getCODE());
+        }
+    }
+
+    //overzicht van alle spelers.
+    public function speler_overview() //: array. heeft andere naam ivm switch van code half door het project
+    {
+        $statement = $this->pdo->prepare('SELECT spelerID AS TYPE, schoolID, roepnaam, tus, achternaam FROM spelers');
+        //$statement->bindParam(//idk wat hier moet :D);
+        $statement->execute();
+        while ($result = $statement->fetch()) {
+            print_r($result);
+        }
+    }
+
+    //overzicht van alle aanmeldingen
+    public function aanmeldingOverzicht() {
+        $statement = $this->pdo->prepare('SELECT aanmeldingID AS TYPE, spelerID, toernooiID FROM aanmeldingen');
+        $statement->execute();
+        while ($result = $statement->fetch()) {
+            print_r($result);
+        }
+    }
+
+    //overzicht van alle toernooien
+    public function toernooiOverzicht() {
+        $statement = $this->pdo->prepare('SELECT toernooiID AS TYPE, omschrijving, datum, updated FROM toernooien');
+        $statement->execute();
+        while ($result = $statement->fetch()) {
+            print_r($result);
+        }
+    }
+
+    //overzicht van alle wedstrijden
+    public function wedstrijdOverzicht() {
+        $statement = $this->pdo->prepare('SELECT wedstrijdID AS TYPE, toernooiID, ronde, speler1, speler2, score1, score2, winnaarID FROM wedstrijden');
+        $statement->execute();
+        while ($result = $statement->fetch()) {
+            print_r($result);
+        }
+    }
+
+    //overzicht van alle scholen
+    public function schoolOverzicht() {
+        $statement = $this->pdo->prepare('SELECT schoolID AS TYPE, naam FROM scholen');
+        $statement->execute();
+        while ($result = $statement->fetch()) {
+            print_r($result);
+        }
+    }
+
+//speler toevoegen WERKT, check DB naar invoeren
+    public function addSpeler($spelerID, $schoolID, $roepnaam, $tus, $achternaam)//: array (niet verwijderen, breekt code XD)
+    {
+        $sql = "INSERT INTO spelers (spelerID, schoolID, roepnaam, tus, achternaam) VALUES (:spelerID, :schoolID, :roepnaam, :tus, :achternaam)";
+        if ($stmt = $this->pdo->prepare($sql)){
+            //linkies
+            $stmt->bindParam(":spelerID", $param_spelerID);
+            $stmt->bindParam(":schoolID", $param_schoolID);
+            $stmt->bindParam(":roepnaam", $param_roepnaam);
+            $stmt->bindParam(":tus", $param_tus);
+            $stmt->bindParam(":achternaam", $param_achternaam);
+
+            //set de parameters
+            $param_spelerID = $spelerID;
+            $param_schoolID = $schoolID;
+            $param_roepnaam = $roepnaam;
+            $param_tus = $tus;
+            $param_achternaam = $achternaam;
+
+            return $stmt->execute();
+
+            if (isset($_POST['addSpeler'])) {
+                $spelerID = $_POST['spelerID'];
+                $schoolID = $_POST['schoolID'];
+                $roepnaam = $_POST['roepnaam'];
+                $tus = $_POST['tus'];
+                $achternaam = $_POST['achternaam'];
+                        
+                //probeer om de statement uivoeren & alt versie indien nodig: $this->statement_execute
+                if($stmt->execute()){
+                //records created succesfull, go back to spelers.php
+                header('Location: spelers.php');
+            } else {
+                echo "Iets ging fout!";
+                }
+            }
+        }
+    }
+
+ //updateSpeler WERKT, CHECK DB
+    public function updateSpeler($spelerID, $schoolID, $roepnaam, $tus, $achternaam)//: array (niet verwijderen, breekt code XD)
+    {
+        $sql = "UPDATE spelers SET spelerID=:spelerID, schoolID=:schoolID, roepnaam=:roepnaam, tus=:tus, achternaam=:achternaam WHERE spelerID=:spelerID";
+         if ($stmt = $this->pdo->prepare($sql)){
+            //linkies
+            $stmt->bindParam(":spelerID", $param_spelerID);
+            $stmt->bindParam(":schoolID", $param_schoolID);
+            $stmt->bindParam(":roepnaam", $param_roepnaam);
+            $stmt->bindParam(":tus", $param_tus);
+            $stmt->bindParam(":achternaam", $param_achternaam);
+
+            //set de parameters
+            $param_spelerID = $spelerID;
+            $param_schoolID = $schoolID;
+            $param_roepnaam = $roepnaam;
+            $param_tus = $tus;
+            $param_achternaam = $achternaam;
+
+            return $stmt->execute();
+
+            if (isset($_POST['updateSpeler'])) {
+                $spelerID = $_POST['spelerID'];
+                $schoolID = $_POST['schoolID'];
+                $roepnaam = $_POST['roepnaam'];
+                $tus = $_POST['tus'];
+                $achternaam = $_POST['achternaam'];
+                        
+                //probeer om de statement uivoeren & alt versie indien nodig: $this->statement_execute
+                if($stmt->execute()){
+                //records created succesfull, go back to spelers.php
+                header('Location: spelers.php');
+            } else {
+                echo "Iets ging fout!";
+                }
+            }
+        }
+    }
+
+//deleteSpeler. Verwijderen van gegevens!
+public function deleteSpeler($spelerID){
+    $sql = "DELETE FROM spelers WHERE spelerID=:spelerID";
+     if ($stmt = $this->pdo->prepare($sql)){
+        //linkies
+        $stmt->bindParam(":spelerID", $param_spelerID);
+
+        //set de parameters
+        $param_spelerID = $spelerID;
+
+        return $stmt->execute();
+
+        if (isset($_POST['deleteSpeler'])) {
+            $spelerID = $_POST['spelerID'];
+                   
+            //probeer om de statement uivoeren & alt versie indien nodig: $this->statement_execute
+            if($stmt->execute()){
+            //records deleted, go back to deletespeler.php
+            header('Location: deletespeler.php');
+        } else {
+            echo "Iets ging fout!";
+            }
+        }
+    }
 }
+
+//toevoegen van Toernooien
+    public function addToernooi($toernooiID, $omschrijving, $datum) //: array
+    {
+        $sql = "INSERT INTO toernooien(toernooiID, omschrijving, datum) VALUES(:toernooiID, :omschrijving, :datum)";
+            if ($stmt = $this->pdo->prepare($sql)) {
+                $stmt->bindParam(":toernooiID",$param_toernooiID);
+                $stmt->bindParam(":omschrijving",$param_omschrijving);
+                $stmt->bindParam(":datum",$param_datum);
+
+                //set de parameters
+                $param_toernooiID = $toernooiID;
+                $param_datum = $datum;
+                $param_omschrijving = $omschrijving;
+
+                return $stmt->execute();
+                
+                if (isset($_POST['addToernooi'])){
+                    $toernooiID = $_POST['toernooiID'];
+                    $omschrijving = $_POST['omschrijving'];
+                    $datum = $_POST['datum'];
+                if($stmt->execute()){
+                    //records created succesfull, go back to spelers.php
+                    header('Location: toernooien.php');
+                    } else {
+                        echo "Iets ging fout!";
+                }
+                           
+            }
+        }   
+    }
+
+//edit Toernooi - editToernooi
+    public function editToernooi($toernooiID, $omschrijving, $datum) //: array
+    {
+        $sql = "UPDATE toernooien SET toernooiID=:toernooiID, omschrijving=:omschrijving, datum=:datum WHERE toernooiID=:toernooiID";   
+            if ($stmt = $this->pdo->prepare($sql)) {
+                $stmt->bindParam(":toernooiID",$param_toernooiID);
+                $stmt->bindParam(":omschrijving",$param_omschrijving);
+                $stmt->bindParam(":datum",$param_datum);
+
+                //set de parameters
+                $param_toernooiID = $toernooiID;
+                $param_datum = $datum;
+                $param_omschrijving = $omschrijving;
+
+                return $stmt->execute();
+                
+                if (isset($_POST['editToernooi'])){
+                    $toernooiID = $_POST['toernooiID'];
+                    $omschrijving = $_POST['omschrijving'];
+                    $datum = $_POST['datum'];
+                if($stmt->execute()){
+                    //records created succesfull
+                    header('Location: toernooien.php');
+                    } else {
+                        echo "Iets ging fout!";
+                    }
+                        
+                }
+            }   
+    }
+
+//deleteToernooi. Verwijderen van gegevens!
+public function deleteToernooi($toernooiID){
+    $sql = "DELETE FROM toernooien WHERE toernooiID=:toernooiID";
+     if ($stmt = $this->pdo->prepare($sql)){
+        //linkies
+        $stmt->bindParam(":toernooiID",$param_toernooiID);
+        $param_toernooiID = $toernooiID;
+        return $stmt->execute();
+
+        if (isset($_POST['deleteToernooi'])) {
+            $spelerID = $_POST['toernooiID'];
+            //probeer om de statement uivoeren & alt versie indien nodig: $this->statement_execute
+            if($stmt->execute()){
+            //records deleted, go back to deletespeler.php
+            header('Location: deletetoernooi.php');
+        } else {
+            echo "Iets ging fout!";
+            }
+        }
+    }
+}
+
+//aanmeldingen toevoegen addAanmelding
+public function addAanmelding($aanmeldingID, $spelerID, $toernooiID){
+    $sql = "INSERT INTO aanmeldingen(aanmeldingID, spelerID, toernooiID) VALUES(:aanmeldingID, :spelerID, :toernooiID)";
+        if ($stmt = $this->pdo->prepare($sql)) {
+
+            $stmt->bindParam(":aanmeldingID",$param_aanmeldingID);
+            $stmt->bindParam(":toernooiID",$param_toernooiID);
+            $stmt->bindParam(":spelerID",$param_spelerID);
+
+            //set de parameters
+            $param_aanmeldingID = $aanmeldingID;
+            $param_spelerID = $spelerID;
+            $param_toernooiID = $toernooiID;
+
+            return $stmt->execute();
+            
+            if (isset($_POST['addAanmelding'])){
+                $aanmeldingID = $_POST['aanmeldingID'];
+                $spelerID = $_POST['spelerID'];
+                $toernooiID = $_POST['toernooiID'];
+            if($stmt->execute()){
+                //records created succesfull, go back.
+                header('Location: aanmeldingen.php');
+                } else {
+                    echo "Iets ging fout!";
+            }
+                       
+        }
+    }   
+}
+
+//update aanmelding updateAanmelding
+public function updateAanmelding($aanmeldingID, $spelerID, $toernooiID){
+    $sql = "UPDATE aanmeldingen SET aanmeldingID=:aanmeldingID, spelerID=:spelerID, toernooiID=:toernooiID WHERE aanmeldingID=:aanmeldingID";
+        if ($stmt = $this->pdo->prepare($sql)) {
+
+            $stmt->bindParam(":aanmeldingID",$param_aanmeldingID);
+            $stmt->bindParam(":toernooiID",$param_toernooiID);
+            $stmt->bindParam(":spelerID",$param_spelerID);
+
+            //set de parameters
+            $param_aanmeldingID = $aanmeldingID;
+            $param_spelerID = $spelerID;
+            $param_toernooiID = $toernooiID;
+
+            return $stmt->execute();
+            
+            if (isset($_POST['updateAanmelding'])){
+                $aanmeldingID = $_POST['aanmeldingID'];
+                $spelerID = $_POST['spelerID'];
+                $toernooiID = $_POST['toernooiID'];
+            if($stmt->execute()){
+                //records created succesfull, go back.
+                header('Location: aanmeldingen.php');
+                } else {
+                    echo "Iets ging fout!";
+            }            
+        }
+    }   
+}
+
+
+//school toevoegen HIER WAS DE TIJD OM, MOET NOG AANPASSEN
+public function addSchool($schoolID, $spelerID, $toernooiID){
+    $sql = "INSERT INTO aanmeldingen(aanmeldingID, spelerID, toernooiID) VALUES(:aanmeldingID, :spelerID, :toernooiID)";
+        if ($stmt = $this->pdo->prepare($sql)) {
+
+            $stmt->bindParam(":aanmeldingID",$param_aanmeldingID);
+            $stmt->bindParam(":toernooiID",$param_toernooiID);
+            $stmt->bindParam(":spelerID",$param_spelerID);
+
+            //set de parameters
+            $param_aanmeldingID = $aanmeldingID;
+            $param_spelerID = $spelerID;
+            $param_toernooiID = $toernooiID;
+
+            return $stmt->execute();
+            
+            if (isset($_POST['addAanmelding'])){
+                $aanmeldingID = $_POST['aanmeldingID'];
+                $spelerID = $_POST['spelerID'];
+                $toernooiID = $_POST['toernooiID'];
+            if($stmt->execute()){
+                //records created succesfull, go back.
+                header('Location: aanmeldingen.php');
+                } else {
+                    echo "Iets ging fout!";
+            }
+                       
+        }
+    }   
+}
+
+
+
+
+};
 
 ?>
---
+++ updatespeler.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+        <img src="logo.png" alt="" width="30" height="24">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="spelers.php">Spelers</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<br><br>
+
+<div>
+  <a href="addspeler.php">Aanmaken</a>
+  <a href="updatespeler.php">Update</a>
+  <a href="deletespeler.php">Verwijderen</a>
+  <a href="spelers.php">Terug</a>
+</div>
+
+<br><br>
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    <input type="text" name="spelerID" placeholder="spelerID" required></input>
+    <input type="text" name="schoolID" placeholder="schoolID" required></input>
+    <input type="text" name="roepnaam" placeholder="roepnaam" required></input>
+    <input type="text" name="tus" placeholder="tus" required></input>
+    <input type="text" name="achternaam" placeholder="achternaam" required></input>
+    <input type="submit" value="submit" name="updateSpeler"></input>
+</form>
+
+<?php   
+//connect to DB
+include 'pdo.php';
+$db = new Database();
+
+//voeg een leeg array toe.
+$spelerID = $schoolID = $roepnaam = $tus = $achternaam = "";
+
+if (isset($_POST['spelerID']) && !empty($_POST['spelerID'])) {
+  $spelerID = $_POST['spelerID'];
+}
+
+//form data wanner submit is gedrukt
+if ($_SERVER ["REQUEST_METHOD"] == "POST") {
+
+    //spelerID toevoegen
+    $imput_spelerID = trim($_POST['spelerID']);
+    if (empty($imput_spelerID)){
+        echo "please enter a id!";
+    } else {
+        $spelerID = $imput_spelerID;
+    }
+
+    //schoolID toevoegen
+    $imput_schoolID = trim($_POST['schoolID']);
+        if (empty($imput_schoolID)){
+            echo "please enter a id!";
+        } else {
+            $schoolID = $imput_schoolID;
+        }
+    
+    //roepnaam toevoegen
+    $imput_roepnaam = trim($_POST['roepnaam']);
+        if (empty($imput_roepnaam)){
+            echo "please enter a roepnaam!";
+        } else {
+            $roepnaam = $imput_roepnaam;
+        }
+    
+    //tus toevoegen
+    $imput_tus = trim($_POST['tus']);
+        if (empty($imput_tus)){
+            echo "please enter a tussennaam!";
+        } else {
+            $tus = $imput_tus;
+        }
+
+    //achternaam toevoegen
+    $imput_achternaam = trim($_POST['achternaam']);
+        if (empty($imput_achternaam)){
+            echo "please enter a achternaam!";
+        } else {
+            $achternaam = $imput_achternaam;
+        }
+}
+
+//de query en gegevens oproepen/invoeren ivm error
+$updateSpeler = $db->updateSpeler($spelerID, $schoolID, $roepnaam, $tus, $achternaam);
+
+?></body></html>
